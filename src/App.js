f8357482ef3be41f9df0b5ad1db0fe67b8512efe
Miifu <mil_la2000@hotmail.com>
2022-10-17 21:18:57 +0300

    Tehtäväpalautus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position =>{
+        console.log(position)
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+        setIsLoading(false)
+
+      }, (error) =>{
+        console.log(error)
+        alert("Paikannus epäonnistui :(")
+      })
+    }else{
+      alert("Selaimesi ei tue paikannusta! >:(")
+    }
+  }, [])
+  
+  if(isLoading){
+    return <p>Ladataan sijaintia...</p>
+  }else{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='content'>
+      <h3>Sinun sijaintisi</h3>
+      <p>
+        Sijainti:&nbsp;
+        {latitude.toFixed(3)},
+        {longitude.toFixed(3)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Weather latitude={latitude} longitude={longitude} />
     </div>
   );
+  }
 }
-
 export default App;
